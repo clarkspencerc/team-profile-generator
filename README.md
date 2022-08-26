6190ef40d03c1be1e4264c6b287ff983cc6cb768
clarkspencerc <clarkspencerc@gmail.com>
2022-08-26 16:35:30 -0600

    css added

--- README.md
+++ README.md
@@ -1,26 +1,37 @@
 # team-profile-generator
 
-AS A manager
-I WANT to generate a webpage that displays my team's basic info
-SO THAT I have quick access to their emails and GitHub profiles
-
-GIVEN a command-line application that accepts user input
-WHEN I am prompted for my team members and their information
-THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-WHEN I click on an email address in the HTML
-THEN my default email program opens and populates the TO field of the email with the address
-WHEN I click on the GitHub username
-THEN that GitHub profile opens in a new tab
-WHEN I start the application
-THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-WHEN I enter the team manager’s name, employee ID, email address, and office number
-THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-WHEN I select the engineer option
-THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-WHEN I select the intern option
-THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-WHEN I decide to finish building my team
-THEN I exit the application, and the HTML is generated
+## Link to Video walkthrough 
+[Link to Video Walkthrough]()
 
 
-to run it the command "node index.js" should be used 
+
+  # Table of Contents
+1. [Description](#description)
+2. [Usage](#usage)
+3. [Contribution](#contribution)
+4. [Test](#test)
+5. [Questions](#questions)
+
+## Description 
+Program that creats a website to see your team, email team members or view your engineers github profiles 
+
+## Usage 
+for anyone who is building out a team and wants to keep their contact info in one place 
+
+## Contribution
+Spencer
+
+## Test
+I tested this out on my own computer multiple times 
+
+## Technologies 
+* Inquierer 
+* fs 
+* Jest
+
+## Learned 
+I learned about correct inquierer syntax as well as how to generate a html based on commandline responses
+
+
+## Questions
+For any questions please contact me at clarkspencerc@gmail.com or find me on github at clarkspencerc
--- src/page-template.js
+++ src/page-template.js
@@ -9,17 +9,19 @@ console.log(team);
 const teamArray = []
 const managerCard = function(employee){
     return `
-    <div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${employee.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${employee.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${employee.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${employee.getOfficeNumber()}</li>
-            </ul>
+    <div class="card" style="width: 18rem">
+        <div class="card employee-card">
+            <div class="card-header text-bg-primary mb-3">
+                <h2 class="card-title">${employee.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${employee.getRole()}</h3>
+            </div>
+            <div class="card-body text-bg-light p-3">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${employee.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
+                    <li class="list-group-item">Office number: ${employee.getOfficeNumber()}</li>
+                </ul>
+            </div>
         </div>
     </div>
     `
@@ -27,24 +29,44 @@ const managerCard = function(employee){
 // make engineer card function as manager card above 
 const engineerCard = function(employee){
     return `
-       <div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${employee.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${employee.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${employee.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
-                <li class="list-group-item">Github: <a href="https://github.com/${employee.getGithub()}">${employee.getGithub()}</a></li>
-            </ul>
+    <div class="card" style="width: 18rem">
+        <div class="card employee-card">
+            <div class="card-header text-bg-primary mb-3">
+                <h2 class="card-title">${employee.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${employee.getRole()}</h3>
+            </div>
+            <div class="card-body text-bg-light p-3">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${employee.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
+                    <li class="list-group-item">Github: <a href="https://github.com/${employee.getGithub()}" target="_blank">${employee.getGithub()}</a></li>
+                </ul>
+            </div>
         </div>
     </div>
     `
 }
 
 // make intern card function as maanger fucntion above 
-
+    const internCard = function (employee) {
+        return `
+       <div class="card" style="width: 18rem">
+        <div class="card employee-card">
+            <div class="card-header text-bg-primary mb-3">
+                <h2 class="card-title">${employee.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${employee.getRole()}</h3>
+            </div>
+            <div class="card-body text-bg-light p-3">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${employee.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
+                    <li class="list-group-item">School: ${employee.getSchool()}</li>
+                </ul>
+            </div>
+        </div>
+        </div>
+    `
+    }
 
 
 teamArray.push(team.filter(employee => employee.getRole() === "Manager")
@@ -57,6 +79,10 @@ teamArray.push(team.filter(employee => employee.getRole() === "Engineer")
 console.log(teamArray);
 
 // push intern to teamArray
+
+teamArray.push(team.filter(employee => employee.getRole() === "Intern")
+.map(employee => internCard(employee))); 
+console.log(teamArray);
 
 
 return teamArray.join('');
@@ -74,17 +100,19 @@ module.exports = team => {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
             <title>The Team</title>
         </head>
 
         <body>
-            <header> 
+            <header class="text-center text-bg-success p-3"> 
                 <h1> The Team </h1>
             </header>
-            <section>
+            <div class="container">
+            <section class="row p-3 justify-content-around m-3">
             ${generatePage(team)}
             </section>
+            </div>
         </body>
     </html>
     
